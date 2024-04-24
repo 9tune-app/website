20ce7fc90627c408797ff52e8c92a6b1a63d3377
sakuramath1997 <takahashi.1997.jp@gmail.com>
2024-04-24 17:08:28 +0900

    added minimal infomation

---
+++ webroot/src/app/legal-notice/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Page() {
+    return(
+        <React.Fragment>
+		<main className="flex min-h-screen flex-col items-center p-24">
+                <h1>特定商取引法に関する表記</h1>
+                <ul>
+                    <li>販売業者: CreateArk Inc.</li>
+                    <li>運営統括責任者: 高橋克典</li>
+                    <li>郵便番号: 〒141-0033</li>
+                    <li>住所: 東京都品川区西品川１丁目１−１ 住友不動産大崎ガーデンタワー 9F</li>
+                    <li>商品代金以外の料金の説明: ありません</li>
+                    <li>お支払い方法: クレジットカード払い</li>
+                    <li>お支払い期限: 月末に翌月分を自動引き落としいたします</li>
+                    <li>サービス名: 9tune</li>
+                </ul>
+            </main>
+        </React.Fragment>
+    );
+}
--- webroot/src/app/page.tsx
+++ webroot/src/app/page.tsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			9tune
+		<main className="flex min-h-screen flex-col items-center p-24">
+			<h1>9tune --クリエイターとミュージシャンとをつなぐプラットフォーム--</h1>
+			<ul>
+				<li>9tuneは、動画クリエイターがYoutubeで使う楽曲を選ぶプラットフォームです。</li>
+				<li>動画クリエイター、ミュージシャンとが共栄する健全な枠組みを目指します。</li>
+			</ul>
+			<h2>はじめかた</h2>
+			<ul>
+				<li>アカウント登録を一週間は体験版が利用できます。</li>
+				<li>動画に使いたい音楽を見つけたら、クリエイター登録をすることで利用できます。</li>
+				<li>ミュージシャンの方は、ミュージシャン登録をする必要があります。</li>
+			</ul>
+			<h2>価格</h2>
+			<ul>
+				<li>月額3000円で、共用楽曲は使いたい放題です。</li>
+			</ul>
+			<h2>リンク</h2>
+			<ul>
+				<li><Link href="./legal-notice">特定商取引法に関する表記</Link></li>
+			</ul>
 		</main>
 	);
 }
